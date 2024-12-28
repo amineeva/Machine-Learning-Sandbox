47bb25c2156bd2edfc5055bb2b23f6ede594cebd
Alex Mineeva <amineeva@olin.edu>
2024-12-28 01:52:30 -0500

    Switched to .ipynb file, finished P1 wine project

--- P1/Main.py
+++ P1/Main.py
@@ -13,6 +13,22 @@ from sklearn.linear_model import LogisticRegression #regularization applied by d
 import warnings
 warnings.filterwarnings('ignore')
 
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+wine_quality = fetch_ucirepo(id=186) 
+  
+# data (as pandas dataframes) 
+X = wine_quality.data.features 
+y = wine_quality.data.targets 
+  
+# metadata 
+print(wine_quality.metadata) 
+  
+# variable information 
+print(wine_quality.variables) 
+
+
 
 # User .describe() to get a description of the dataset
 # To get a feel for the dataset create 2D and 3D plots of various features (scatter plots)
